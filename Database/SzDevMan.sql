2e92c07031c8626804ae2398a0f8b4cdcd8e9429
jakobSeyrlehner <jakob.seyrlehner@sz-ybbs.ac.at>
2020-12-14 14:54:37 +0100

    databaseChanges

--- Database/SzDevMan.sql
+++ Database/SzDevMan.sql
@@ -1,5 +1,12 @@
+DROP DATABASE IF EXISTS SzDevMan;
+CREATE DATABASE SzDevMan;
+
+show databases;
+use SzDevMan;
+
+drop table if exists Device; 
 CREATE TABLE Device(
-    Id INT AUTO_INCREMENT,
+    DeviceId INT AUTO_INCREMENT,
     Type VARCHAR(255),
     Manufacturer VARCHAR(255),
     DeviceName VARCHAR(255),
@@ -11,26 +18,41 @@ CREATE TABLE Device(
     DateOfPurchase DATE,
     Warranty VARCHAR(255),
     InventoryId VARCHAR(255),
-    PRIMARY KEY (Id)
+    PRIMARY KEY (DeviceId)
 );
+insert into Device (type, Manufacturer, DeviceName, DeviceVersion, DeviceSerialNumber, YearOfProduction) VALUES ('PC', 'Asus', 'PC1', '1.0', '1234', 2002);
 
+drop table if exists Service; 
 CREATE TABLE Service(
-    Id INT AUTO_INCREMENT,
+    ServiceId INT AUTO_INCREMENT,
     Type VARCHAR(255),
     ServiceName VARCHAR(255),
     ServiceVersion VARCHAR(255),
     ServiceLicence VARCHAR(255),
     ServiceProvider VARCHAR(255),
     ServiceAccess TEXT,
-    PRIMARY KEY (Id)
+    PRIMARY KEY (ServiceId)
 );
 
+drop table if exists Software; 
 CREATE TABLE Software(
-    Id INT AUTO_INCREMENT,
+    SoftwareId INT AUTO_INCREMENT,
     Type VARCHAR(255),
     SoftwareName VARCHAR(255),
     SoftwareVersion VARCHAR(255),
     SoftwareLicence VARCHAR(255),
-    Hardware VARCHAR(255),
-    PRIMARY KEY (Id)
+    HardwareId INT,
+    PRIMARY KEY (SoftwareId),
+    CONSTRAINT fk_SoftwareDevice FOREIGN KEY (HardwareId) REFERENCES Device(DeviceId) ON UPDATE CASCADE ON DELETE CASCADE
 );
+insert into Software (type, SoftwareName, SoftwareVersion, SoftwareLicence, HardwareId) VALUES ('SW1', 'Photoshop', '14.1', 'Yes', 1);
+
+
+show tables;
+select * from device;
+select * from Service;
+select * from Software;
+
+
+
+
