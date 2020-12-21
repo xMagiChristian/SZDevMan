8cf32e82b189b78231e77ca2987a424190621a87
xMagiChristian <cheesi.gamsjaeger@gmail.com>
2020-12-21 13:46:18 +0100

    new SLIM Framework

---
+++ REST_API/SZDevManSLIM/src/Action/HomeAction.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Action;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class HomeAction
+{
+    public function __invoke(
+        ServerRequestInterface $request, 
+        ResponseInterface $response
+    ): ResponseInterface {
+        $response->getBody()->write(json_encode(['success' => true]));
+
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
